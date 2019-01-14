9cee6abe491c88b021764e74556e2b836d3d7ed3
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-01-14 15:13:27 -0500

    The code has been cracked

--- drills-2.js
+++ drills-2.js
@@ -15,3 +15,39 @@ function beyond(num) {
     console.log('Staying home');
   }
 }
+
+
+const decode = function (cypherText) {
+
+  const dec = function (word) {
+
+    switch (word[0]) {
+
+      case 'a':
+        return word[1];
+        break;
+
+      case 'b':
+        return word[2];
+        break;
+
+      case 'c':
+        return word[3];
+        break;
+
+      case 'd':
+        return word[4];
+        break;
+
+      default:
+        return ' ';
+        break;
+    }
+  };
+
+  return cypherText.split(' ').map(dec).join('');
+
+};
+
+
+console.log(decode('craft block argon meter bells brown croon droop'));
