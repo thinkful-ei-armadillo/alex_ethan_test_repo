4fa5a808a150fbd2b366cb6e72d06679720336d7
AReach <DerAReich@gmail.com>
2019-01-14 13:47:46 -0600

    added beyond function

--- drills-2.js
+++ drills-2.js
@@ -3,3 +3,17 @@
 function jediName(firstName, lastName) {
   return firstName.slice(0,3)+lastName.slice(0,2);
 }
+
+function beyond(num) {
+  if (Number.isInfinite(num)) {
+    console.log('And beyond');
+  } else if (num > 0) {
+    console.log('To infinity');
+  } else if (num < 0) {
+    console.log('To negative infinity');
+  } else  {
+    console.log('Staying home');
+  }
+}
+
+beyond(5);
